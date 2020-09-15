942884a72ee96d422df321577d43b20e754c6f5c
Marlon Earle <marlonearle@MacBook-Air.local>
2020-09-15 11:11:08 -0400

    first commit

---
+++ intro.js
@@ -0,0 +1,15 @@
+console.log('Hello World!');
+
+// Primitive Data Types: 
+        // Definition: Primitive Data Types ARE NOT mutale
+    // Strings:
+    // Numbers:
+    // Boolean:
+    // Null:
+    // Undefined: is a primitive value just defined 
+
+// Non-Primitive Data Types:
+        // Definition: Non-Primitive DAta Types ARE mutale
+    // Objects
+    // Arrays
+    // Functions
