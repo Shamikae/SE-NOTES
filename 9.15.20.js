42ad99b22ba1ec2356b475a46724cddd55177b8e
Marlon Earle <e.shamika@yahoo.com>
2020-09-16 11:45:56 -0400

    String notes

--- 9.15.20.js
+++ 9.15.20.js
@@ -1,1 +1,2 @@
 console.log('Hello World from 9.15.20!');
+
---
+++ day3exercises.js
@@ -0,0 +1,40 @@
+// Define a new variable and set it equal to the string of your name
+    // Log this to the console
+// Define a new variable and set it equal to a number 
+    // Log this to the console
+// Set a variable to a boolean value 
+    // Log this to the console
+
+var name = 'Shamika';
+console.log(name);
+
+var num  = 20;
+console.log(num)
+
+var value= true;
+console.log(value); 
+
+// 4. Create a variable called carName, assign the value Volvo to it.
+    var carName = 'Volvo';
+    console.log(carName)
+
+// 5. Create a variable called num, assign the value 50 to it.
+    var num = 50;
+    console.log(num)
+// 6. Display the sum of 5 + 10, using two variables: num1 and num2.
+    var num1 = 5;
+    var num2 = 10;
+    console.log(num1 + num2)
+// 7. Create a variable called num3, assign the addition of num1 and num2 to it.
+    var num3 = num1 + num2;
+    console.log(num3)
+// 8. Create a set of variables that define an animal (age, height, name, etc...). Must be atleast 6 variables.
+    var animal = 'whale';
+    var name = 'Willy';
+    var age = 8;
+    var length = '30ft';
+    var wings = false;
+    var blowhole = true;
+
+    var sentence = `My animal is a ${animal}.
+    console.log(sentence)
