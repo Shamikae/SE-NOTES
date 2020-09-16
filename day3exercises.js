6ae46e8138723a23493a62a703ff35d14a253942
Marlon Earle <e.shamika@yahoo.com>
2020-09-16 16:13:52 -0400

    Added string exercises

--- day3exercises.js
+++ day3exercises.js
@@ -1,40 +1,60 @@
-// Define a new variable and set it equal to the string of your name
-    // Log this to the console
-// Define a new variable and set it equal to a number 
-    // Log this to the console
-// Set a variable to a boolean value 
-    // Log this to the console
+// // Define a new variable and set it equal to the string of your name
+//     // Log this to the console
+// // Define a new variable and set it equal to a number 
+//     // Log this to the console
+// // Set a variable to a boolean value 
+//     // Log this to the console
 
-var name = 'Shamika';
-console.log(name);
+// var name = 'Shamika';
+// console.log(name);
 
-var num  = 20;
-console.log(num)
+// var num  = 20;
+// console.log(num)
 
-var value= true;
-console.log(value); 
+// var value= true;
+// console.log(value); 
 
-// 4. Create a variable called carName, assign the value Volvo to it.
-    var carName = 'Volvo';
-    console.log(carName)
+// // 4. Create a variable called carName, assign the value Volvo to it.
+//     var carName = 'Volvo';
+//     console.log(carName)
 
-// 5. Create a variable called num, assign the value 50 to it.
-    var num = 50;
-    console.log(num)
-// 6. Display the sum of 5 + 10, using two variables: num1 and num2.
-    var num1 = 5;
-    var num2 = 10;
-    console.log(num1 + num2)
-// 7. Create a variable called num3, assign the addition of num1 and num2 to it.
-    var num3 = num1 + num2;
-    console.log(num3)
-// 8. Create a set of variables that define an animal (age, height, name, etc...). Must be atleast 6 variables.
-    var animal = 'whale';
-    var name = 'Willy';
-    var age = 8;
-    var length = '30ft';
-    var wings = false;
-    var blowhole = true;
+// // 5. Create a variable called num, assign the value 50 to it.
+//     var num = 50;
+//     console.log(num)
+// // 6. Display the sum of 5 + 10, using two variables: num1 and num2.
+//     var num1 = 5;
+//     var num2 = 10;
+//     console.log(num1 + num2)
+// // 7. Create a variable called num3, assign the addition of num1 and num2 to it.
+//     var num3 = num1 + num2;
+//     console.log(num3)
+// // 8. Create a set of variables that define an animal (age, height, name, etc...). Must be atleast 6 variables.
+//     var animal = 'whale';
+//     var name = 'Willy';
+//     var age = 8;
+//     var length = '30ft';
+//     var wings = false;
+//     var blowhole = true;
 
-    var sentence = `My animal is a ${animal}.
-    console.log(sentence)
+//     var sentence = `My animal is a ${animal}.
+//     console.log(sentence);
+
+
+    // level 0
+    // Q0: Declare a string with the value euqal to your name
+    var name = 'Shamika';
+    // Q1: Given the string, 'WE ARE SHOUTING!' log 'we are shouting'
+    console.log('WE ARE SHOUTING!'.toLowerCase() == 'we are shouting'.toLowerCase()) 
+// level 1
+    // Q2: Declare a string and log its length 
+    var str1 = 'Tiger';
+    console.log(str1.length)
+    // Q3: Given the string 'Hello World!', log the 6th character.
+    console.log('Hello World!'[5])
+    console.log()
+// level 2
+    // Q4: Given the string 'Welcome World?', replace the question mark with an exclamation
+
+
+// level 3
+    // given any string, log the first and last characters
