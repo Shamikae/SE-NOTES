adbe6fc9ec3cc9735f346ac04d86db7e07c784ee
Shamika Earle <e.shamika@yahoo.com>
2020-09-17 05:00:52 -0400

    Added string exercises

---
+++ day3assignment.js
@@ -0,0 +1,37 @@
+// var car = 'BMW';
+// var model = 'i8';
+// var year = 2021;
+// var body = '2 Door';
+// var color = 'Blue';
+// var interior = 'Tan'
+// var convertible = true;
+// var engine = '3 cylinder';
+// var electric = false;
+// var hybrid = true;
+
+// var sentence = `My dream car is a ${car} ${model}. I want a brand new ${year} ${body}. Exterior ${color} with ${interior} inside. The best part is the turbocharged ${engine}. `;
+// console.log(sentence);
+
+var fulltext1 = "MAN54681;Manchester Piccadilly";
+
+var fulltext2 = "GNF6519871681;Greenfield"
+
+var fulltext3 = "LIV515;Liverpool"
+
+var fulltext4 = "SYB98981981;Stalybridge"
+
+var fulltext5 = "HUD517819181;Huddersfield"
+
+var sentence = `Code: ${fulltext1.split(';'), fulltext1.replace(';',', City: ')}`
+var sentence1 = `Code: ${fulltext2.split(';'), fulltext2.replace(';',', City: ')}`
+var sentence2 = `Code: ${fulltext3.split(';'), fulltext3.replace(';',', City: ')}`
+var sentence3 = `Code: ${fulltext4.split(';'), fulltext4.replace(';',', City: ')}`
+var sentence4 = `Code: ${fulltext5.split(';'), fulltext5.replace(';',', City: ')}`
+
+    console.log(sentence);
+    console.log(sentence1);
+    console.log(sentence2);
+    console.log(sentence3);
+    console.log(sentence4);
+
+
